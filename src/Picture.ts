f4f603fc5c9f6bbc8e4c4ac85da970003c287ac7
Gabriel Castro Muñoz <gabcas28@gmail.com>
2020-10-26 15:49:18 +0100

    Updating Picture Interface #6 #7

--- src/Picture.ts
+++ src/Picture.ts
@@ -1,39 +1,17 @@
 class Picture {
-    id;
-    username;
-    title;
-    description;
-    constructor(id = 0, username = 0, title: String = '', description: String = '') {
-        this.id = id;
-        this.username = username;
-        this.title = title;
-        this.description = description;
-    }
-    getId() {
-        return true;
-    }
-    getUsername() {
-        return true;
-    }
-    setUsername(username) {
-        return username
-    }
-    getTitle() {
-        return true;
-    }
-    setTitle(title) {
-        return title;
-    }
-    getDescription() {
-        return true;
-    }
-    setDescription(description) {
-        return description;
-    }
-    save() {
-        return true;
-    };
-    delete() {
-        return true;
-    };
+  id;
+  username;
+  title;
+  description;
+  constructor(
+    id = undefined,
+    username = undefined,
+    title: String = "",
+    description: String = ""
+  ) {}
+
+  save() {}
+  read() {}
+  update() {}
+  delete() {}
 }
