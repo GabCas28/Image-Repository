49c1d5f5411312904cbebe37cc226277bfb2c1e6
Gabriel Castro Muñoz <gabcas28@gmail.com>
2020-10-26 15:51:15 +0100

    Updating Review Interface #9

--- src/Review.ts
+++ src/Review.ts
@@ -1,39 +1,16 @@
 class Review {
-    id;
-    username;
-    title;
-    content;
-    constructor(id = '', username = '', title: String = '', content: String = '') {
-        this.id = id;
-        this.username = username;
-        this.title = title;
-        this.content = content;
-    }
-    getId() {
-        return true;
-    }
-    getUsername() {
-        return true;
-    }
-    setUsername(username) {
-        return username;
-    }
-    getTitle() {
-        return true;
-    }
-    setTitle(title) {
-        return title;
-    }
-    getContent() {
-        return true;
-    }
-    setContent(content) {
-        return content
-    }
-    save() {
-        return true;
-    };
-    delete() {
-        return true;
-    };
+  id;
+  username;
+  title;
+  content;
+  constructor(
+    id = undefined,
+    username = undefined,
+    title: String = "",
+    content: String = ""
+  ) {}
+  save() {}
+  read() {}
+  update() {}
+  delete() {}
 }
