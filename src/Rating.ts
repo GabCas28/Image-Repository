5f7d0d521ffa645f06d734b5aa3f2d5e54d1f96d
Gabriel Castro Muñoz <gabcas28@gmail.com>
2020-10-26 15:49:44 +0100

    Updating Rating Interface #8

--- src/Rating.ts
+++ src/Rating.ts
@@ -1,36 +1,17 @@
 class Rating {
-    id;
-    username;
-    stars;
-    is_liked;
-    constructor(id = "", username = '', stars: Number = -1, is_liked: Boolean = false) {
-        this.id = id;
-        this.username = username;
-        this.stars = stars;
-        this.is_liked = is_liked;
-    }
-    getId() {
-        return true;
-    }
-    getUsername() {
-        return true;
-    }
-    setUsername(username) {
-        return username;
-    }
-    getStars() {
-        return true;
-    }
-    getIsLiked() {
-        return true;
-    }
-    setIsLiked(is_liked) {
-        return is_liked;
-    }
-    save() {
-        return true;
-    };
-    delete() {
-        return true;
-    };
+  id;
+  username;
+  stars;
+  is_liked;
+  constructor(
+    id = undefined,
+    username = undefined,
+    stars: Number = -1,
+    is_liked: Boolean = false
+  ) {}
+
+  save() {}
+  read() {}
+  update() {}
+  delete() {}
 }
