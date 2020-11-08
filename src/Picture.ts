3835cb0f70f75172faadf157417110b755bba8b5
Gabriel Castro Muñoz <gabcas28@gmail.com>
2020-11-08 23:45:36 +0100

    Updates Picture class, closes #34

--- src/Picture.ts
+++ src/Picture.ts
@@ -1,11 +1,19 @@
-class Picture {
-	constructor(
-		id,
-		author: User,
-		title: String,
-		description: String,
-		source: String,
-		comments: Comment[],
-		ratings: Rating[]
-	) {}
+import { Rating } from "./Rating";
+import { Review } from "./Review";
+import { User } from "./User";
+
+export class Picture {
+  author: User;
+  title: String;
+  description: String;
+  source: String;
+  reviews: Review[];
+  ratings: Rating[];
+
+  constructor(
+    author: User,
+    title: String,
+    description: String,
+    source: String
+  ) {}
 }
