44232d1c569113da27d5fcf0bd7118c551b5064a
Gabriel Castro Muñoz <gabcas28@gmail.com>
2020-11-12 02:10:56 +0100

    Create constructor for Rating class and check its behaviour. Closes #39

--- src/Rating.ts
+++ src/Rating.ts
@@ -3,7 +3,13 @@ import { User } from "./User";
 export class Rating {
   author: User;
   value: Number;
-  
-  constructor(author: User, value: Number) {}
-  
+
+  constructor(author: User, value: Number) {
+    this.author = author;
+    this.value = value;
+  }
+
+  toString() {
+    return `${this.author} - Rating:${this.value}`;
+  }
 }
---
+++ src/tests/rating.test.ts
@@ -0,0 +1,22 @@
+import { assert } from "chai";
+import { User } from "../User";
+import { Rating } from "../Rating";
+
+describe("Unit Tests Rating.ts", function () {
+  var test_user = new User("Gabriel", "Castro", "test@gmail.com", "password");
+
+  // Testea que se haya cargado bien la librería
+  describe("Load source file: " + __dirname + "/../Rating.ts", function () {
+    it("Should be loaded", function () {
+      assert.ok(Rating, "Loaded");
+    });
+  });
+
+  describe("Create", function () {
+    it("Should create Ratings correctly", function () {
+      var rating_value = 5;
+      var new_Rating = new Rating(test_user, rating_value);
+      assert.equal(new_Rating.toString(), `Gabriel Castro - test@gmail.com - Rating:5`);
+    });
+  });
+});
