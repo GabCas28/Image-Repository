eb531ea5e26f47ec876d0b9c7b0f6e3d8479fa18
Gabriel Castro Muñoz <gabcas28@gmail.com>
2020-11-12 02:12:30 +0100

    Creates Review constructor and tests its behaviour. Closes #40

--- src/Review.ts
+++ src/Review.ts
@@ -5,6 +5,18 @@ export class Review {
   title: String;
   body: String;
   votes: Number;
-  
-  constructor(author: User, title: String, body: String) {}
+
+  constructor(author: User, title: String, body: String) {
+    this.author = author;
+    this.title = title;
+    this.body = body;
+    this.votes = 0;
+  }
+
+  toString(): String {
+    return `${this.author}:\n${this.title}\n${this.body.slice(
+      0,
+      80
+    )}...\nVotes:${this.votes}`;
+  }
 }
---
+++ src/tests/review.test.ts
@@ -0,0 +1,29 @@
+import { assert } from "chai";
+import { User } from "../User";
+import { Review } from "../Review";
+
+describe("Unit Tests Review.ts", function () {
+  var test_user = new User("Gabriel", "Castro", "test@gmail.com", "password");
+
+  // Testea que se haya cargado bien la librería
+  describe("Load source file: " + __dirname + "/../Review.ts", function () {
+    it("Should be loaded", function () {
+      assert.ok(Review, "Loaded");
+    });
+  });
+
+  describe("Create", function () {
+    it("Should create Reviews correctly", function () {
+      var title =
+        "Enim veniam do officia elit consequat in eiusmod magna dolor incididunt cupidatat tempor ad.";
+      var body =
+        "Deserunt ullamco sunt proident qui dolore reprehenderit officia commodo cillum dolore in irure. Pariatur esse quis cillum ea est sint pariatur sunt officia. Tempor cillum ullamco culpa laboris culpa culpa quis qui cillum irure nostrud sit nisi. Reprehenderit irure adipisicing do laborum adipisicing aliquip ea nulla nulla mollit eiusmod.";
+
+      var new_Review = new Review(test_user, title, body);
+      assert.equal(
+        new_Review.toString(),
+        `Gabriel Castro - test@gmail.com:\nEnim veniam do officia elit consequat in eiusmod magna dolor incididunt cupidatat tempor ad.\nDeserunt ullamco sunt proident qui dolore reprehenderit officia commodo cillum d...\nVotes:0`
+      );
+    });
+  });
+});
