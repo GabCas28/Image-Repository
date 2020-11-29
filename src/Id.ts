ceccac3da16ffacddd321a7e168dd1562fc3113b
Gabriel Castro Muñoz <gabcas28@gmail.com>
2020-11-29 03:18:56 +0100

    Modified Id class and tests to use UUID instead of a random number. Closes #54

--- src/Id.ts
+++ src/Id.ts
@@ -1,17 +1,34 @@
+import {
+  version as uuidVersion,
+  validate as uuidValidate,
+  v4 as uuid_v4,
+} from "uuid";
+
 export class Id {
-  private id: String;
+  private id: string;
 
   get [Symbol.toStringTag]() {
     return "Id";
   }
-  constructor(id?: String) {
+  constructor(id?: string) {
+    if (id) {
+      this.validateId(id);
+      this.checkVersion(id);
+    }
     this.id = id ? id : this.generateId();
   }
-  private generateId(): String {
-    return `${Math.floor(Math.random() * 10 ** 16)}`;
+  private generateId(): string {
+    return uuid_v4();
   }
-  valueOf() {
-    return this.id;
+  private validateId(str: string): void {
+    if (!uuidValidate(str)) {
+      throw new Error("Invalid Id");
+    }
+  }
+  private checkVersion(str: string): void {
+    if (uuidVersion(str) != 4) {
+      throw new Error("Invalid UUID version");
+    }
   }
   toString() {
     return `${this.id}`;
--- tests/id.test.ts
+++ tests/id.test.ts
@@ -1,8 +1,13 @@
 import { assert, expect } from "chai";
 import { Id } from "../src/Id";
+import {
+  version as uuidVersion,
+  validate as uuidValidate,
+  v4 as uuid_v4,
+  v1 as uuid_v1,
+} from "uuid";
 
 describe("Unit Tests Id.ts", function () {
-  // Testea que se haya cargado bien la librería
   describe(`Load source file: ${__dirname}/../src/Id.ts`, function () {
     it("Should be loaded", function () {
       assert.ok(Id, "Loaded");
@@ -11,15 +16,16 @@ describe("Unit Tests Id.ts", function () {
 
   describe("Create", function () {
     it("Should create Id correctly", function () {
-      var new_id = new Id();
+      let new_id = new Id();
       expect(new_id).to.be.an("Id", "The object is not an Id");
     });
 
     it("Should print Id correctly", function () {
-      var new_id = new Id("12");
+      let new_uuid = uuid_v4();
+      let new_id = new Id(new_uuid);
       assert.equal(
         new_id.toString(),
-        "12",
+        new_uuid,
         "The id printed is not as expected"
       );
     });
@@ -29,5 +35,29 @@ describe("Unit Tests Id.ts", function () {
       assert.notDeepEqual(new_id.valueOf(), "", "The id hasn't been generated");
     });
 
+    it("Should be an UUID the id generated ", function () {
+      var new_id = new Id();
+      assert.ok(
+        uuidValidate(new_id.toString()),
+        "The id generated is not a valid UUID"
+      );
+    });
+
+    it("Should be a version 4 UUID ", function () {
+      var new_id = new Id();
+      assert.equal(
+        uuidVersion(new_id.toString()),
+        4,
+        "The UUID is not version 4"
+      );
+    });
+
+    it("Should throw an error when passing an invalid Id ", function () {
+      assert.throws(() => new Id("12"));
+    });
+
+    it("Should throw an error when passing an UUID from different versions ", function () {
+      assert.throws(() => new Id(uuid_v1()));
+    });
   });
 });
