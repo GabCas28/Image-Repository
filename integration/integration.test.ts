49aaf1381149685477087c3c80186eae21bd62f7
Gabriel Castro <gabcas28@gmail.com>
2021-02-11 09:17:11 +0200

    fix integration.tests

--- integration/integration.test.ts
+++ integration/integration.test.ts
@@ -1,37 +1,52 @@
-import { assert } from "chai";
+import { assert } from 'chai';
 var request = require('superagent');
-describe("Integration Tests Image-Repo", function () {
+describe('Integration Tests Image-Repo', function() {
+	describe('Docker Connection', function() {
+		// it("Should connect to the gateway", function () {
+		//   return request.get("gateway:5000/pictures").then((res:any)=>{
+		//     assert.ok(res, "not connected")
+		//   }).catch((err:any)=>{throw err})
+		// });
+		// it("Should connect to the pictures", function () {
+		//   return request.get("pictures:3600/pictures").then((res:any)=>{
+		//     assert.ok(res, "not connected")
+		//   }).catch((err:any)=>{throw err})
+		// });
 
-  describe("Docker Connection", function () {
-    // it("Should connect to the gateway", function () {
-    //   return request.get("gateway:5000/pictures").then((res:any)=>{
-    //     assert.ok(res, "not connected")
-    //   }).catch((err:any)=>{throw err})
-    // });
-    // it("Should connect to the pictures", function () {
-    //   return request.get("pictures:3600/pictures").then((res:any)=>{
-    //     assert.ok(res, "not connected")
-    //   }).catch((err:any)=>{throw err})
-    // });
-    it("Should connect to the gateway", function () {
-      return request.get("gateway:5000/pictures").then((res: any) => {
-        assert.ok(res, "not connected")
-      }).catch((err: any) => {
-        return request.get("localhost:5000/pictures").then((res: any) => {
-          assert.ok(res, "not connected")
-        }).catch((err: any) => { throw err })
-      })
-    });
-    it("Should connect to the pictures", function () {
-      return request.get("pictures:3600/pictures").then((res: any) => {
-        assert.ok(res, "not connected")
-      }).catch((err: any) => {
-        return request.get("localhost:5000/pictures").then((res: any) => {
-          assert.ok(res, "not connected")
-        }).catch((err: any) => { throw err })})
-    });
-  });
-  });
-  
+		it('Should connect to the gateway', function() {
+			return request
+				.get('gateway:5000/pictures')
+				.then((res: any) => {
+					assert.ok(res, 'not connected');
+				})
+				.catch((err: any) => {
+					return request
+						.get('localhost:5000/pictures')
+						.then((res: any) => {
+							assert.ok(res, 'not connected');
+						})
+						.catch((err: any) => {
+							throw err;
+						});
+				});
+		});
 
+		it('Should connect to the pictures', function() {
+			return request
+				.get('pictures:3600/pictures')
+				.then((res: any) => {
+					assert.ok(res, 'not connected');
+				})
+				.catch((err: any) => {
+					return request
+						.get('localhost:5000/pictures')
+						.then((res: any) => {
+							assert.ok(res, 'not connected');
+						})
+						.catch((err: any) => {
+							throw err;
+						});
+				});
+		});
+	});
 });
